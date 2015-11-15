d990de162576565cec35cd7a0d676b53f4c1430b
llvc <llvc1986@gmail.com>
2015-11-15 08:35:43 +0100

    source

---
+++ Gp2uTests/PhotoconTests.h
@@ -0,0 +1,13 @@
+//
+//  PhotoconTests.h
+//  PhotoconTests
+//
+//  Created by LionUser on 9/24/14.
+//  Copyright (c) 2014 choe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PhotoconTests : SenTestCase
+
+@end
---
+++ Gp2uTests/PhotoconTests.m
@@ -0,0 +1,32 @@
+//
+//  PhotoconTests.m
+//  PhotoconTests
+//
+//  Created by LionUser on 9/24/14.
+//  Copyright (c) 2014 choe. All rights reserved.
+//
+
+#import "PhotoconTests.h"
+
+@implementation PhotoconTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PhotoconTests");
+}
+
+@end
